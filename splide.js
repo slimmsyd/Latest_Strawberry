e3072a8fcd6d8d21fb387c75cacded2e33c44497
Sydney Sanders <ssanderss444@gmail.com>
2023-02-13 14:21:21 -0500

    first commit

---
+++ splide.js
@@ -0,0 +1,46 @@
+
+// const splide =  new Splide( '.splide', {
+//     type    : 'loop',
+//     autoplay: 'play',
+//   } )
+
+//   splide.mount()
+
+
+
+
+
+
+
+
+
+
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    spaceBetween: 800,
+    autoplay: {
+      delay: 5000,
+    },
+    allowTouchMove: false,
+
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+    },
+  });
